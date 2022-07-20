21a17d3ad87a102bd9c7213c8775b04658b0de84
Nathaniel Davydov <nathanielmdavydov@gmail.com>
2022-07-20 11:01:36 -0400

    commit tree 4596c2713c6a0bc6cfb8217eb35c6566014a1c50

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,89 @@
 # your code goes here
+require 'pry'
+class Person
+    attr_reader :name, :happiness, :hygiene
+    attr_writer 
+    attr_accessor :bank_account
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(value)
+        if value > 10 
+            @happiness = 10
+        elsif value < 0 
+            @happiness = 0
+        else 
+            @happiness = value
+        end
+    end
+
+    def hygiene=(value)
+        if value > 10 
+            @hygiene = 10
+        elsif value < 0 
+            @hygiene = 0
+        else 
+            @hygiene = value
+        end
+    end
+
+    def clean?
+        if @hygiene > 7
+            return true
+        else 
+            false
+        end
+    end
+
+    def happy?
+        if @happiness > 7
+            return true
+        else 
+            false
+        end
+    end
+
+    def get_paid(amount)
+        @bank_account += amount
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene = @hygiene + 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness = @happiness + 2
+        self.hygiene = @hygiene - 3
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness = @happiness + 3
+        friend.happiness = friend.happiness + 3
+        
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            binding.pry
+            friend.happiness = friend.happiness - 2
+            self.happiness = @happiness - 2
+            return "blah blah partisan blah lobbyist"    
+        elsif topic == "weather"
+            friend.happiness = friend.happiness + 1
+            self.happiness = @happiness + 1
+            return "blah blah sun blah rain"
+        else
+            return "blah blah blah blah blah"
+        end
+    end
+
+end
